1f8d36607222943f53f312f1d7e0f5a561caed14
jaguillo <juloo.dsi@gmail.com>
2014-12-26 22:13:11 +0100

    4 params check

--- Norme.py
+++ Norme.py
@@ -6,19 +6,43 @@ import sublime, sublime_plugin
 class JulooNorme(sublime_plugin.EventListener):
 
 	def on_post_save_async(self, view):
-		functions = view.find_by_selector("meta.function.c meta.block.c")
 		invalids = []
-		if len(functions) > 5:
+#
+# 5 functions per file
+#
+		regions = view.find_by_selector("meta.function.c entity.name.function.c")
+		if len(regions) > 5:
 			i = 5
-			while i < len(functions):
-				invalids.append(functions[i])
+			while i < len(regions):
+				invalids.append(regions[i])
 				i += 1
-			print("Norme Error: " + str(len(functions)) + " functions")
-		for f in functions:
-			a = view.rowcol(f.begin())
-			b = view.rowcol(f.end())
+			print("Norme Error: " + str(len(regions)) + " functions")
+#
+# 25 lines per function
+#
+		regions = view.find_by_selector("meta.function.c meta.block.c")
+		for r in regions:
+			a = view.rowcol(r.begin())
+			b = view.rowcol(r.end())
 			if b[0] - a[0] >= 27:
 				invalids.append(sublime.Region(view.text_point(a[0] + 26, a[1]),
-					view.text_point(b[0] - 1, b[1])))
+					view.text_point(b[0], b[1] - 1)))
 				print("Norme Error: " + str(b[0] - a[0] - 1) + " lines in a function")
-		view.add_regions("norme_errors", invalids, "invalid.norme", "circle", sublime.DRAW_NO_OUTLINE)
+#
+# 4 function params
+#
+		regions = view.find_by_selector("meta.function.c meta.parens.c");
+		for r in regions:
+			params = view.substr(r).split(",");
+			if len(params) > 4:
+				l = 0;
+				i = 0;
+				while i < 4:
+					l += len(params[i]) + 1;
+					i += 1
+				r = sublime.Region(r.begin() + l, r.end() - 1)
+				invalids.append(r)
+#
+# End
+#
+		view.add_regions("norme_errors", invalids, "source invalid.norme", "circle", sublime.DRAW_NO_OUTLINE)
