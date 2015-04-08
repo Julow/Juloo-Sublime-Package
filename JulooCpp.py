55dac5f02e726f1c077cca629b9ae23ec7c30d1d
jaguillo <juloo.dsi@gmail.com>
2015-04-08 14:37:52 +0200

    Canonical form

--- JulooCpp.py
+++ JulooCpp.py
@@ -6,7 +6,7 @@
 #    By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/04/07 14:14:38 by jaguillo          #+#    #+#              #
-#    Updated: 2015/04/07 14:46:07 by jaguillo         ###   ########.fr        #
+#    Updated: 2015/04/08 14:37:22 by jaguillo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,34 +16,48 @@ class JulooCppCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit, **args):
 		pattern = """
-#include "%s"
+#include "%(hpp)s"
 
-%s::%s(void)
+%(class)s::%(class)s(void)
 {
 }
 
-%s::~%s(void)
+%(class)s::%(class)s(%(class)s const &src)
 {
+	*this = src;
+}
+
+%(class)s::~%(class)s(void)
+{
+}
+
+%(class)s			&operator=(%(class)s const &rhs)
+{
+	*this = src;
+	return (*this);
 }
 """
 		name = self.view.file_name().split('/')[-1]
 		className = name.split('.')[0]
 		name = name.replace(".cpp", ".hpp")
-		self.view.run_command("juloo_write", {"point": 0, "data": pattern % (name, className, className, className, className)})
+		self.view.run_command("juloo_write", {"point": 0, "data": pattern % {'hpp': name, 'class': className}})
 		self.view.run_command("juloo_header", {"action": "insert"})
 
 class JulooHppCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit, **args):
 		pattern = """
-#ifndef %s
-# define %s
+#ifndef %(hdef)s
+# define %(hdef)s
 
-class	%s
+class	%(class)s
 {
 public:
-	%s(void);
-	~%s(void);
+	%(class)s(void);
+	%(class)s(%(class)s const &src);
+	~%(class)s(void);
+
+	%(class)s			&operator=(%(class)s const &rhs);
 
 private:
 };
@@ -53,5 +67,5 @@ private:
 		name = self.view.file_name().split('/')[-1]
 		className = name.split('.')[0]
 		name = ''.join([i if ord(i) >= ord('A') and ord(i) <= ord('Z') else '_' for i in name.upper()])
-		self.view.run_command("juloo_write", {"point": 0, "data": pattern % (name, name, className, className, className)})
+		self.view.run_command("juloo_write", {"point": 0, "data": pattern % {'hdef': name, 'class': className}})
 		self.view.run_command("juloo_header", {"action": "insert"})
