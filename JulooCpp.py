a7e835f5247338c808b0e0a03c843b3e27b8fe2f
jaguillo <juloo.dsi@gmail.com>
2015-04-17 12:51:28 +0200

    Improve cpp/hpp

--- JulooCpp.py
+++ JulooCpp.py
@@ -6,7 +6,7 @@
 #    By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/04/07 14:14:38 by jaguillo          #+#    #+#              #
-#    Updated: 2015/04/13 23:06:14 by jaguillo         ###   ########.fr        #
+#    Updated: 2015/04/17 12:49:56 by jaguillo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,59 +15,75 @@ import sublime, sublime_plugin
 class JulooCppCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit, **args):
-		pattern = """
-#include "%(hpp)s"
+		pattern = """#include "%(hpp)s"
+// #include <iostream>
 
 %(class)s::%(class)s(void)
 {
-}
-
-%(class)s::%(class)s(%(class)s const &src)
-{
-	*this = src;
 }
 
 %(class)s::~%(class)s(void)
 {
 }
 
-%(class)s			&%(class)s::operator=(%(class)s const &rhs)
-{
-	// *this = rhs;
-	return (*this);
-}
+// %(class)s::%(class)s(%(class)s const &src)
+// {
+// 	*this = src;
+// }
+
+// %(class)s			&%(class)s::operator=(%(class)s const &rhs)
+// {
+// 	// *this = rhs;
+// 	return (*this);
+// }
+
+// std::ostream		&operator<<(std::ostream &o, %(class)s const &rhs)
+// {
+// 	std::cout << rhs << std::endl;
+// 	return (o);
+// }
 """
+		self.view.run_command("juloo_header", {"action": "insert"})
 		name = self.view.file_name().split('/')[-1]
 		className = name.split('.')[0]
 		name = name.replace(".cpp", ".hpp")
-		self.view.run_command("juloo_write", {"point": 0, "data": pattern % {'hpp': name, 'class': className}})
-		self.view.run_command("juloo_header", {"action": "insert"})
+		self.view.run_command("juloo_write", {
+			"point": self.view.sel()[0].begin(),
+			"data": pattern % {'hpp': name, 'class': className}
+		})
 
 class JulooHppCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit, **args):
-		pattern = """
-#ifndef %(hdef)s
+		pattern = """#ifndef %(hdef)s
 # define %(hdef)s
+
+// # include <ostream>
+// # include <string>
+// # include <exception>
 
 class	%(class)s
 {
 public:
 	%(class)s(void);
-	%(class)s(%(class)s const &src);
 	virtual ~%(class)s(void);
-
-	%(class)s			&operator=(%(class)s const &rhs);
 
 protected:
 
 private:
+	%(class)s(%(class)s const &src);
+	%(class)s			&operator=(%(class)s const &rhs);
 };
+
+// std::ostream		&operator<<(std::ostream &o, %(class)s const &rhs);
 
 #endif
 """
+		self.view.run_command("juloo_header", {"action": "insert"})
 		name = self.view.file_name().split('/')[-1]
 		className = name.split('.')[0]
 		name = ''.join([i if ord(i) >= ord('A') and ord(i) <= ord('Z') else '_' for i in name.upper()])
-		self.view.run_command("juloo_write", {"point": 0, "data": pattern % {'hdef': name, 'class': className}})
-		self.view.run_command("juloo_header", {"action": "insert"})
+		self.view.run_command("juloo_write", {
+			"point": self.view.sel()[0].begin(),
+			"data": pattern % {'hdef': name, 'class': className}
+		})
