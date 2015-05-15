18b34fcf2cd938a0cfab4e0cc09f2626451d9472
jaguillo <juloo.dsi@gmail.com>
2015-05-15 17:37:58 +0200

    Improve JulooCpp (enum + interface + remove useless)

--- JulooCpp.py
+++ JulooCpp.py
@@ -6,17 +6,13 @@
 #    By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/04/07 14:14:38 by jaguillo          #+#    #+#              #
-#    Updated: 2015/04/17 12:49:56 by jaguillo         ###   ########.fr        #
+#    Updated: 2015/05/15 17:38:05 by jaguillo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sublime, sublime_plugin
 
-class JulooCppCommand(sublime_plugin.TextCommand):
-
-	def run(self, edit, **args):
-		pattern = """#include "%(hpp)s"
-// #include <iostream>
+c_class_template = """#include "%(hpp)s"
 
 %(class)s::%(class)s(void)
 {
@@ -36,31 +32,22 @@ class JulooCppCommand(sublime_plugin.TextCommand):
 // 	// *this = rhs;
 // 	return (*this);
 // }
-
-// std::ostream		&operator<<(std::ostream &o, %(class)s const &rhs)
-// {
-// 	std::cout << rhs << std::endl;
-// 	return (o);
-// }
 """
+
+class JulooCppCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit, **args):
 		self.view.run_command("juloo_header", {"action": "insert"})
 		name = self.view.file_name().split('/')[-1]
 		className = name.split('.')[0]
 		name = name.replace(".cpp", ".hpp")
 		self.view.run_command("juloo_write", {
 			"point": self.view.sel()[0].begin(),
-			"data": pattern % {'hpp': name, 'class': className}
+			"data": c_class_template % {'hpp': name, 'class': className}
 		})
 
-class JulooHppCommand(sublime_plugin.TextCommand):
-
-	def run(self, edit, **args):
-		pattern = """#ifndef %(hdef)s
+h_class_template = """#ifndef %(hdef)s
 # define %(hdef)s
-
-// # include <ostream>
-// # include <string>
-// # include <exception>
 
 class	%(class)s
 {
@@ -75,15 +62,49 @@ private:
 	%(class)s			&operator=(%(class)s const &rhs);
 };
 
-// std::ostream		&operator<<(std::ostream &o, %(class)s const &rhs);
+#endif
+"""
+
+h_interface_template = """#ifndef %(hdef)s
+# define %(hdef)s
+
+class	%(class)s
+{
+public:
+	virtual ~%(class)s(void){}
+
+protected:
+
+private:
+};
 
 #endif
 """
+
+h_enum_template = """#ifndef %(hdef)s
+# define %(hdef)s
+
+enum class	%(class)s
+{
+};
+
+#endif
+"""
+
+class JulooHppCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit, **args):
 		self.view.run_command("juloo_header", {"action": "insert"})
 		name = self.view.file_name().split('/')[-1]
 		className = name.split('.')[0]
-		name = ''.join([i if ord(i) >= ord('A') and ord(i) <= ord('Z') else '_' for i in name.upper()])
+		hdef = ''.join([i if ord(i) >= ord('A') and ord(i) <= ord('Z') else '_' for i in name.upper()])
+		template = h_class_template
+		if ord(className[1]) >= ord('A') and ord(i) <= ord('Z'):
+			if className[0] == 'I':
+				template = h_interface_template
+			elif className[0] == 'E':
+				template = h_enum_template
 		self.view.run_command("juloo_write", {
 			"point": self.view.sel()[0].begin(),
-			"data": pattern % {'hdef': name, 'class': className}
+			"data": template % {'hdef': hdef, 'class': className}
 		})
