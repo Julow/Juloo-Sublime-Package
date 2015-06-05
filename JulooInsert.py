86e3a76681b147c417884ebc9e97bc1426d2303f
jaguillo <juloo.dsi@gmail.com>
2015-06-05 13:28:00 +0200

    Add default cmd + Fix with python 2

--- JulooInsert.py
+++ JulooInsert.py
@@ -6,14 +6,17 @@
 #    By: juloo <juloo@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/05/21 23:31:22 by juloo             #+#    #+#              #
-#    Updated: 2015/05/22 12:53:25 by jaguillo         ###   ########.fr        #
+#    Updated: 2015/06/05 13:20:48 by jaguillo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sublime_plugin, sublime
 from subprocess import Popen, PIPE, STDOUT
 from os import path
-from shlex import quote
+try:
+    from shlex import quote
+except ImportError:
+    from pipes import quote
 
 shell_query = """cd %(path)s
 export FILE=%(file_name)s
@@ -22,6 +25,8 @@ export COLUMN=%(column)d
 export TEXT=%(text)s
 %(cmd)s
 """
+
+default_cmd = "echo \"$TEXT\" | "
 
 class JulooInsertCommand(sublime_plugin.TextCommand):
 
@@ -39,7 +44,7 @@ class JulooInsertCommand(sublime_plugin.TextCommand):
 				"column": self.view.rowcol(s.begin())[1] + 1,
 				"text": quote(self.view.substr(text_region)),
 				"cmd": cmd
-			}, 'UTF-8'))
+			}))
 			self.view.run_command("juloo_write", {
 				"region": {"begin": s.begin(), "end": s.end()},
 				"data": process.communicate()[0].decode('UTF-8').strip('\n')
@@ -59,4 +64,4 @@ class JulooInsertCommand(sublime_plugin.TextCommand):
 					"data": "%d" % self.view.rowcol(s.begin())[1] + 1
 				})
 		elif args["what"] == "shell":
-			self.view.window().show_input_panel("Shell command", "", self._insert_shell, None, None)
+			self.view.window().show_input_panel("Shell command", default_cmd, self._insert_shell, None, None)
