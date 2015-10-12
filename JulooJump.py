9f23d8398e2faf58784f384a12aec02cfadc136f
jaguillo <juloo.dsi@gmail.com>
2015-10-12 22:55:30 +0200

    JulooJump

--- JulooJump.py
+++ JulooJump.py
@@ -6,7 +6,7 @@
 #    By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/10/12 19:25:59 by jaguillo          #+#    #+#              #
-#    Updated: 2015/10/12 20:14:50 by jaguillo         ###   ########.fr        #
+#    Updated: 2015/10/12 22:53:29 by juloo            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,10 +15,10 @@ import sublime, sublime_plugin
 #
 # JulooJump
 #
-# Jump by 8 lines
+# Jump by n lines
 # TODO: Jump to next declaration
 #
-JUMP_BY = 8
+JUMP_BY = 5
 
 class JulooJumpCommand(sublime_plugin.TextCommand):
 
@@ -29,21 +29,25 @@ class JulooJumpCommand(sublime_plugin.TextCommand):
 	def jump_by_lines(self, by, shift):
 		sels = []
 		for s in self.view.sel():
-			pt = self.jump_pt(s.a, by)
+			pt = self.jump_pt(s.b, by)
 			if not shift:
 				sels.append(sublime.Region(pt, pt))
 			elif by < 0:
-				sels.append(sublime.Region(pt, s.end()))
+				sels.append(sublime.Region(s.a, pt))
 			else:
-				sels.append(sublime.Region(s.begin(), pt))
+				sels.append(sublime.Region(s.a, pt))
 		self.view.sel().clear()
 		for s in sels:
 			self.view.sel().add(s)
+		if by < 0:
+			self.view.show(sels[0])
+		else:
+			self.view.show(sels[-1])
 
 	def run(self, edit, **args):
 		if not "action" in args:
 			return
 		if args["action"] == "up":
-				self.jump_by_lines(-JUMP_BY, "shift" in args)
+			self.jump_by_lines(-JUMP_BY, "shift" in args)
 		elif args["action"] == "down":
-				self.jump_by_lines(JUMP_BY, "shift" in args)
+			self.jump_by_lines(JUMP_BY, "shift" in args)
