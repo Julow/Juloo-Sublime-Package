983d83f9403f853f2f9a00cedbdcd6c5b9b86fe5
jaguillo <juloo.dsi@gmail.com>
2015-12-01 12:47:20 +0100

    Do not print header and hdef if file is not empty

--- JulooCpp.py
+++ JulooCpp.py
@@ -6,7 +6,7 @@
 #    By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/04/07 14:14:38 by jaguillo          #+#    #+#              #
-#    Updated: 2015/05/22 14:48:05 by jaguillo         ###   ########.fr        #
+#    Updated: 2015/12/01 12:46:29 by jaguillo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,9 +22,20 @@ c_class_template = """#include "%(hpp)s"
 {
 }
 
+// %(class)s::%(class)s(%(class)s &&src)
+// {
+// 	*this = src;
+// }
+
 // %(class)s::%(class)s(%(class)s const &src)
 // {
 // 	*this = src;
+// }
+
+// %(class)s			&%(class)s::operator=(%(class)s &&rhs)
+// {
+// 	// *this = rhs;
+// 	return (*this);
 // }
 
 // %(class)s			&%(class)s::operator=(%(class)s const &rhs)
@@ -46,10 +57,15 @@ class JulooCppCommand(sublime_plugin.TextCommand):
 			"data": c_class_template % {'hpp': name, 'class': className}
 		})
 
-h_class_template = """#ifndef %(hdef)s
+hdef_before = """#ifndef %(hdef)s
 # define %(hdef)s
 
-class	%(class)s
+"""
+
+hdef_after = """
+#endif"""
+
+h_class_template = """class	%(class)s
 {
 public:
 	%(class)s(void);
@@ -58,17 +74,14 @@ public:
 protected:
 
 private:
-	%(class)s(%(class)s const &src);
-	%(class)s			&operator=(%(class)s const &rhs);
+	%(class)s(%(class)s &&src) = delete;
+	%(class)s(%(class)s const &src) = delete;
+	%(class)s			&operator=(%(class)s &&rhs) = delete;
+	%(class)s			&operator=(%(class)s const &rhs) = delete;
 };
-
-#endif
 """
 
-h_interface_template = """#ifndef %(hdef)s
-# define %(hdef)s
-
-class	%(class)s
+h_interface_template = """class	%(class)s
 {
 public:
 	virtual ~%(class)s(void){}
@@ -77,34 +90,34 @@ protected:
 
 private:
 };
-
-#endif
 """
 
-h_enum_template = """#ifndef %(hdef)s
-# define %(hdef)s
-
-enum class	%(class)s
+h_enum_template = """enum class	%(class)s
 {
 };
-
-#endif
 """
 
 class JulooHppCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit, **args):
-		self.view.run_command("juloo_header", {"action": "insert"})
-		name = self.view.file_name().split('/')[-1]
+		hdef_enabled = self.view.sel()[0].begin() <= 0
+		name = (self.view.file_name() or "Class").split('/')[-1]
+		data = ""
+		if hdef_enabled:
+			self.view.run_command("juloo_header", {"action": "insert"})
+			hdef = ''.join([i if ord(i) >= ord('A') and ord(i) <= ord('Z') else '_' for i in name.upper()])
+			data += hdef_before % {'hdef': hdef}
 		className = name.split('.')[0]
-		hdef = ''.join([i if ord(i) >= ord('A') and ord(i) <= ord('Z') else '_' for i in name.upper()])
 		template = h_class_template
 		if ord(className[1]) >= ord('A') and ord(className[1]) <= ord('Z'):
 			if className[0] == 'I':
 				template = h_interface_template
 			elif className[0] == 'E':
 				template = h_enum_template
+		data += template % {'class': className}
+		if hdef_enabled:
+			data += hdef_after % {'hdef': hdef}
 		self.view.run_command("juloo_write", {
 			"point": self.view.sel()[0].begin(),
-			"data": template % {'hdef': hdef, 'class': className}
+			"data": data
 		})
