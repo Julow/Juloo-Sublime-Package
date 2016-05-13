552e6ae639383f7801fa3e74b10a5252963c1b2e
jaguillo <juloo.dsi@gmail.com>
2016-05-13 18:14:33 +0200

    Rewrite cursor plugin

--- cursor.py
+++ cursor.py
@@ -1,12 +1,12 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    JulooCursor.py                                     :+:      :+:    :+:    #
+#    cursor.py                                          :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: juloo <juloo@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/08/30 16:57:29 by juloo             #+#    #+#              #
-#    Updated: 2015/12/01 14:15:46 by jaguillo         ###   ########.fr        #
+#    Updated: 2016/05/13 18:13:53 by jaguillo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,64 +21,92 @@ import sublime, sublime_plugin
 #
 # Multi cursor:
 #
-# ctrl+shift+down			Add a cursor 1 row below (disabled)
-# ctrl+shift+up				Add a cursor 1 row above (disabled)
+# ctrl+shift+down			Add a cursor 1 row below
+# ctrl+shift+up				Add a cursor 1 row above
 #
 
-savedSelections = []
+ACTIONS = {
+	"save": lambda s: s.save_cursors(),
+	"restore": lambda s: s.restore_cursors(),
+	"clear": lambda s: s.clear_cursors(),
+	"add_up": lambda s: s.add_cursor_column(False),
+	"add_down": lambda s: s.add_cursor_column(True)
+}
+
+SAVED_REGIONS_KEY = "juloo_saved_cursors"
+SAVED_REGIONS_SCOPE = "comment"
+SAVED_REGIONS_FLAGS = sublime.DRAW_EMPTY | sublime.DRAW_NO_FILL | sublime.PERSISTENT
 
 TAB_SIZE = 4
 
-# add cursor
-def tabs_rowcol(view, pt):
-	row, _ = view.rowcol(pt)
-	line = view.substr(sublime.Region(view.text_point(row, 0), pt)).expandtabs(TAB_SIZE)
-	return (row, len(line))
-
-def tabs_text_point(view, row, col):
-	pt = view.text_point(row, 0)
-	expanded = len(view.substr(sublime.Region(pt, view.text_point(row, col))).expandtabs(TAB_SIZE))
-	return view.text_point(row, col - (expanded - col))
-
-def next_line_fit(view, row, col, by, sel_size):
-	# if sel_size == 0:
-	# 	line_len = len(view.substr(view.line(view.text_point(row, 0))).expandtabs(TAB_SIZE))
-	# 	return tabs_text_point(view, row, col)
-	min_len = col + sel_size
-	max_row, _ = view.rowcol(view.size())
-	while row >= 0 and row <= max_row:
-		line_len = len(view.substr(view.line(view.text_point(row, 0))).expandtabs(TAB_SIZE))
-		if line_len >= min_len:
-			return tabs_text_point(view, row, col)
-		row += by
-	return -1
-
-def add_cursor(view, by):
-	to_add = []
-	for s in view.sel():
-		row, col = tabs_rowcol(view, s.begin())
-		pt = next_line_fit(view, row + by, col, by, s.size())
-		if pt < 0:
-			continue
-		to_add.append(sublime.Region(pt, pt + s.size()))
-	for s in to_add:
-		view.sel().add(s)
-# -
-
-def action_save(view):
-	for s in view.sel():
-		savedSelections.append(s)
-
-actions = {
-	"save": action_save,
-	"restore": lambda view: view.sel().add_all(savedSelections),
-	"clear": lambda view: savedSelections.clear(),
-	"add_up": lambda view: add_cursor(view, -1),
-	"add_down": lambda view: add_cursor(view, 1)
-}
-
 class JulooCursorCommand(sublime_plugin.TextCommand):
 
+	def __init__(self, p):
+		sublime_plugin.TextCommand.__init__(self, p)
+
 	def run(self, edit, **args):
-		if "action" in args and args["action"] in actions:
-			actions[args["action"]](self.view)
+		if "action" in args and args["action"] in ACTIONS:
+			ACTIONS[args["action"]](self)
+
+	# Save current cursors
+	def save_cursors(self):
+		r = self.view.get_regions(SAVED_REGIONS_KEY) + list(self.view.sel())
+		self.view.add_regions(SAVED_REGIONS_KEY, r, SAVED_REGIONS_SCOPE, flags=SAVED_REGIONS_FLAGS)
+
+	# Clear saved cursors
+	def clear_cursors(self):
+		self.view.add_regions(SAVED_REGIONS_KEY, [])
+
+	# Restore saved cursors
+	def restore_cursors(self):
+		self.view.sel().add_all(self.view.get_regions(SAVED_REGIONS_KEY))
+		self.clear_cursors()
+
+	# Add a cursor by column
+	def add_cursor_column(self, down):
+		def sels():
+			for s in self.view.sel():
+				while self.view.rowcol(s.a)[0] != self.view.rowcol(s.b)[0]:
+					line = self.view.line(s.begin())
+					yield sublime.Region(s.begin(), line.end())
+					s = sublime.Region(line.end(), s.end())
+				yield s
+
+		def expand_tabs(s, end=-1):
+			i = 0
+			l = 0
+			if end < 0:
+				end = len(s) * 4
+			while i < len(s) and l < end:
+				l += 1 if s[i] != '\t' else TAB_SIZE - (l % TAB_SIZE)
+				i += 1
+			return i, l
+
+		def tab_rowcol(pt):
+			row, _ = self.view.rowcol(pt)
+			_, line = expand_tabs(self.view.substr(sublime.Region(self.view.text_point(row, 0), pt)))
+			return (row, line)
+		def tab_text_point(row, col):
+			pt = self.view.text_point(row, 0)
+			expanded, _ = expand_tabs(self.view.substr(self.view.line(pt)), col)
+			return pt + expanded
+
+		def inc_row(r):
+			row, col = tab_rowcol(r.begin())
+			size = r.size()
+			while True:
+				row += 1 if down else -1
+				lstart = tab_text_point(row, 0)
+				_, llen = expand_tabs(self.view.substr(self.view.line(lstart)))
+				if llen >= col:
+					size = min(size, llen - col)
+					break
+				if lstart == 0 or lstart >= self.view.size():
+					return None
+			begin = tab_text_point(row, col)
+			a, b = (begin, begin + size) if r.a < r.b else (begin + size, begin)
+			return sublime.Region(a, b)
+
+		s = inc_row((max if down else min)(sels()))
+		if s != None:
+			self.view.sel().add(s)
