e8c54d6006abcd89702e389a3f15716daf591cb0
jaguillo <juloo.dsi@gmail.com>
2016-07-18 17:01:59 +0200

    ctrl+e/ctrl+y scroll by 5 lines

--- scroll.py
+++ scroll.py
@@ -6,7 +6,7 @@
 #    By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/07/18 15:46:31 by jaguillo          #+#    #+#              #
-#    Updated: 2016/07/18 15:53:58 by jaguillo         ###   ########.fr        #
+#    Updated: 2016/07/18 17:00:24 by jaguillo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,8 +17,11 @@ class JulooScrollCommand(sublime_plugin.WindowCommand):
 	def run(self, **args):
 		view = self.window.active_view()
 		_, v_height = view.viewport_extent()
-		offset = v_height / 2 if args.get("half_page", False) else 1
+		if args.get("half_page", False):
+			offset = v_height / 3
+		else:
+			offset = args.get("offset", 1) * view.line_height()
 		if args.get("rev", False):
 			offset = -offset
 		x, y = view.viewport_position()
-		view.set_viewport_position((x, y + offset))
+		view.set_viewport_position((x, max(y + offset, 0)))
