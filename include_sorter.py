df9d887faaf6a6b738be812819df8d2d99096798
juloo <juloo.dsi@gmail.com>
2018-07-10 11:51:56 +0200

    Rewrite include_sorter

--- include_sorter.py
+++ include_sorter.py
@@ -1,78 +1,33 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    include_sorter.py                                  :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2015/12/11 14:06:40 by jaguillo          #+#    #+#              #
-#    Updated: 2016/06/20 18:51:17 by jaguillo         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 import sublime_plugin, sublime, re
 
-#
-# ============================================================================ #
-# C/C++
-#
+# C includes
+# Sorted by:
+#	- quotes ([""] then [<>])
+# 	- prefix (part before any [/], empty first)
+#	- file name
+def c_include(m):
+	quote, name = (1, m[0]) if m[0] else (0, m[1])
+	prefix, _, name = name.rpartition("/")
+	return (quote, prefix, name)
 
-REG_C_INCLUDE = re.compile('\s*#\s*include\s*(?:<([^>]+)>|"([^"]+)").*')
-
-def is_c_include(line):
-	m = REG_C_INCLUDE.match(line)
-	if m == None:
-		return None
-	if m.group(1) == None:
-		return (m.group(0), m.group(2), False)
-	return (m.group(0), m.group(1), True)
-
-def c_include_sort(includes):
-	simple_prefixed, simple, angle, angle_prefixed = {}, {}, {}, {}
-	for i in includes:
-		if "/" in i[1]:
-			dst = angle_prefixed if i[2] else simple_prefixed
-		else:
-			dst = angle if i[2] else simple
-		dst[i[1]] = i
-	def sort(a):
-		return [a[i][0] for i in sorted(a)]
-	def helper(a, b):
-		if len(b) == 0:
-			return a
-		elif len(a) == 0:
-			return b
-		return a + [""] + b if len(a) > 1 or len(b) > 1 else a + b
-	return helper(helper(sort(simple_prefixed), sort(simple)), helper(sort(angle), sort(angle_prefixed)))
-
-#
-# ============================================================================ #
-# module
-#
-
-REG_MODULE_REQUIRE = re.compile('\s*(?:(public|private)\s+|)require\s+([^\s]+).*')
-
-def is_module_require(line):
-	m = REG_MODULE_REQUIRE.match(line)
-	return (m.group(0), m.group(1) == "public", m.group(2)) if m != None else None
-
-def module_require_sort(lines):
-	def sort(lines):
-		return [l[0] for l in sorted(lines, key=lambda l: (not l[1], "::" not in l[2], l[2]))]
-	public, private = [], []
-	for l in lines:
-		(public if l[1] else private).append(l)
-	public, private = sort(public), sort(private)
-	split = [""] if len(public) > 0 and len(private) > 0 else []
-	return public + split + private
+# makemake's module files require
+# Sorted by:
+#	- visibility ([public] then [private])
+#	- namespace (empty goes first)
+#	- name
+def module_require(m):
+	visi = ["public", "private", None].index(m[0])
+	name = m[1]
+	prefix, _, name = name.rpartition("::")
+	return (visi, prefix, name)
 
 #
 # ============================================================================ #
 #
 
 LANGS = [
-	(["C++"], [], is_c_include, c_include_sort),
-	([], ["module"], is_module_require, module_require_sort)
+	(["C++"], [], '\s*#\s*include\s*(?:<([^>]+)>|"([^"]+)").*', c_include),
+	([], ["module"], '\s*(?:(public|private)\s+|)require\s+([^\s]+).*', module_require)
 ]
 
 def get_lang(file_name, syntax):
@@ -83,32 +38,35 @@ def get_lang(file_name, syntax):
 		for n in lang[1]:
 			if file_name.endswith(n):
 				return (lang[2], lang[3])
-	raise Exception("Unsupported language %s" % syntax)
+	return None
 
-def sort_includes(view, edit):
-	is_include, include_sort = get_lang(view.file_name(), view.settings().get("syntax"))
+# Sort the "includes" of in a view
+# [read_include], given a line, must returns the key used to sort
+# 	or None if its not an include
+def sort_includes(read_include, view, edit):
 	max_row, _ = view.rowcol(view.size())
-	includes = None
-	def helper(includes, eof=False):
-		if includes != None:
-			lines = include_sort(includes[0])
-			r = sublime.Region(view.text_point(includes[1], 0),
-				view.text_point(row, 0) - 1)
-			view.replace(edit, r, "\n".join(lines) + ("\n" if not eof else ""))
-	for row in range(max_row + 1):
-		line = view.substr(view.line(view.text_point(row, 0)))
-		if len(line.strip()) == 0:
-			continue
-		inc = is_include(line)
-		if inc == None:
-			helper(includes)
-			includes = None
-		else:
-			if includes == None:
-				includes = ([], row)
-			print ("INCLUDE %s" % line)
-			includes[0].append(inc)
-	helper(includes, True)
+	def chunks():
+		lines = []
+		chunk_begin = None
+		chunk_end = None
+		for row in range(max_row + 1):
+			line_region = view.line(view.text_point(row, 0))
+			line = view.substr(line_region)
+			key = read_include(line)
+			if key is None:
+				if len(lines) > 0:
+					yield lines, sublime.Region(chunk_begin, chunk_end)
+					lines = []
+			else:
+				if len(lines) == 0:
+					chunk_begin = line_region.begin()
+				lines.append((line, key))
+				chunk_end = line_region.end()
+		if len(lines) > 0:
+			yield lines, sublime.Region(chunk_begin, chunk_end)
+	for lines, region in chunks():
+		lines = map(lambda k: k[0], sorted(lines, key=lambda k: k[1]))
+		view.replace(edit, region, "\n".join(lines))
 
 class JulooIncludeSorter(sublime_plugin.EventListener):
 
@@ -116,11 +74,15 @@ class JulooIncludeSorter(sublime_plugin.EventListener):
 		if view.settings().get("juloo_sort_include", True):
 			view.run_command("juloo_sort_include")
 
-
 class JulooSortInclude(sublime_plugin.TextCommand):
 
 	def run(self, edit, **args):
-		try:
-			sort_includes(self.view, edit)
-		except Exception as e:
-			print("JulooIncludeSort: %s" % str(e))
+		v = self.view
+		lang = get_lang(v.file_name(), v.settings().get("syntax"))
+		if lang is not None:
+			reg, read = lang
+			reg = re.compile(reg)
+			def read_include(line):
+				m = reg.match(line)
+				return None if m is None else read(m.groups())
+			sort_includes(read_include, v, edit)
